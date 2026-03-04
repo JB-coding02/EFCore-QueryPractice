e691e0cb46ba0a5887607cf9df27e4329d3cef73
Justin <justinburns2008@gmail.com>
2026-03-04 14:56:05 -0800

    Added Query 1

--- EFCore-QueryPractice/Program.cs
+++ EFCore-QueryPractice/Program.cs
@@ -2,4 +2,19 @@
 // Place a Console.Readkey() after each query to pause the console window so you can see the results.
 // Use a Console.WriteLine() to print the query question before the result.
 
-Console.WriteLine("Query practice!");
+using EFCore_QueryPractice.Models;
+using Microsoft.EntityFrameworkCore;
+
+ApContext myApContext = new();
+
+// Query 1: Retrieve all invoices along with their associated vendor information. The include performs a "Join" in SQL.
+List<Invoice> allInvoices = 
+    await myApContext
+        .Invoices
+        .Include(invoice => invoice.Vendor)
+        .ToListAsync();
+
+foreach (var invoice in allInvoices)
+{
+    Console.WriteLine($"{invoice.InvoiceNumber} {invoice.Vendor.VendorName}");
+}
